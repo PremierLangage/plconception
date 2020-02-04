8d559b355c6ffa07bf6ee4323520ac8cdd1037fd
Dominique Revuz <nimdanor@github.com>
2020-02-04 11:20:09 +0100

    from makall

---
+++ conception/concept/makepdf.sh
@@ -0,0 +1,19 @@
+rm all.md nrm.md readme.md
+
+echo "\tableofcontents" >>all.tutu
+echo "\section{Glossaire}" >>all.tutu 
+for i in *.md 
+do
+echo "<!-- $i -->" >>all.tutu
+cat $i| sed -e "s/^#/##/" >>all.tutu
+echo "1. [$i]($i)" >>nrm.md
+done
+
+mv nrm.md readme.md 
+mv all.tutu all.md
+pandoc -V geometry:margin=1.5cm all.md -o all.pdf
+
+rm all.md nrm.md 
+
+
+
