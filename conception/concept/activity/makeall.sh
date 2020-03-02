505464ed7dd03421e083a8f9f2fcb71acf587de4
Dominique Revuz <nimdanor@github.com>
2020-03-02 18:19:53 +0100

    cReatioini

--- conception/concept/activity/makeall.sh
+++ conception/concept/activity/makeall.sh
@@ -3,13 +3,23 @@
 rm all.md nrm_md readme.md
 
 echo "## Activity " >>all.tutu
-for i in *.md 
+for j in standard types plugapps 
 do
-echo "<!-- $i -->" >>all.tutu
+echo "### **$j**" >>all.tutu 
+echo $j/readme.md >>all.tutu
+for i in $j/*.md
+do
+if test $i == $j/readme.md
+then
+echo $i
+else
+echo "<!-- $j $i -->" >>all.tutu
 echo "<a id=\"$i\"></a>" >>all.tutu
 echo >>all.tutu
 cat $i| sed -e "s/^#/###/"  >>all.tutu
 echo >>all.tutu
+fi
+done
 done
 
 mv all.tutu all.md
