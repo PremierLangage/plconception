26809295486f0bdb93b34b91abb4644ea1e431ea
Dominique Revuz <dominique.revuz@gmail.com>
2020-04-24 21:52:30 +0200

    Create publication.pl

---
+++ conception/concept/publication.pl
@@ -0,0 +1,10 @@
+
+
+# Publication 
+
+La publication est l'acte de rendre public une ressource pour pouvoir la placer dans un cours.
+
+La publication ce fait dans un cercle donnée, la resource et toutes ces ressources associé qui ne sont pas déjà publiées, 
+sont publiées dans l'atelier du cercle dans la partie publique.
+
+
